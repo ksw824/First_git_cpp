ab3035cd526885dba2cfeb7c8a3f7e788f90f01f
Sunwoo Kim <tjsdnrla1234@gmail.com>
2022-11-18 15:56:38 +0900

    The first git Cpp

---
+++ First.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1,  num2;
+
+    cout << "정수 1 입력: ";
+    cin >> num1;
+    cout << " 정수 2 입력: ";
+    cin >> num2;
+
+    cout << "num1 + num2 = " << num1 + num2 << endl;
+    cout << "num1 - num2 = " << num1 - num2 << endl;
+    return 0;
+}
+
